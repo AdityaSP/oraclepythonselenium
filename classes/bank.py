10c0082e113ed56e9545754682a805970c55e9d9
Aditya <sp.aditya@gmail.com>
2017-10-10 15:30:19 +0530

    Update bank.py

--- classes/bank.py
+++ classes/bank.py
@@ -1,10 +1,35 @@
 ''' My Bank software'''
+
 class Account(object):
     def __init__(self, n, b, t):
         self.n = n
         self.b = b
         self.t = t
+    def debit(self, amount):
+        self.b -= amount
+        
+    def credit(self, amount):
+        self.b += amount
+
+class CurrentAccount(Account):
+    ''' Current Account Class handles CA'''
+    def __init__(self, n, b):
+        Account.__init__(self,n,b,'C')
+
     def __repr__(self):
-        return 'Account({},{})'.format(self.n, self.b)
-    def __str__(self):
-        return '[Account -> {},{}]'.format(self.n, self.b)
+        return 'CA({},{})'.format(self.n, self.b)
+
+        
+class SavingsAccount(Account):
+    ''' Base Account Class handles everythin'''
+    def __init__(self, n, b):
+        Account.__init__(self,n,b,'S')
+
+    def __repr__(self):
+        return 'SA({},{})'.format(self.n, self.b)
+        
+    def debit(self, amount):
+        if self.b < amount :
+            raise ValueError("Insufficient Balance")
+        else :    
+            Account.debit(self,amount)
